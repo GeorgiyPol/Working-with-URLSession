c665dafbf8b6051e7a1438498aa1cae651db52ee
Georgiy <33875528+georg1856@users.noreply.github.com>
2022-07-27 16:41:26 +1000

    Create fish struct

---
+++ URLSession.playground/Sources/Fish Struct.swift
@@ -0,0 +1,10 @@
+import Foundation
+
+public struct FishStruct {
+    
+    public var fishScheme = "https"
+    public var fishHost = "fishwatch.gov"
+    public var fishPath = "/api/species/american-lobster"
+    
+    public init() {}
+}
